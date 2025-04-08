d9f536a3b48fb8c71cddaac786a3265c3a8402a4
edoubart <edouard.barthelemy@gmail.com>
2025-04-08 23:38:48 +0200

    Add foreground image.

--- src/screens/GameOver/index.js
+++ src/screens/GameOver/index.js
@@ -1,13 +1,28 @@
 // NPM Packages
-import { Text, View } from 'react-native';
+import { Image, Text, View } from 'react-native';
+
+// Custom Modules
+import {
+  Title,
+} from './../../components/ui';
 
 // Styles
 import styles from './styles';
 
+// Constants
+const IMAGE_FOREGROUND_PATH = './../../../assets/images/success.png';
+
 function GameOver() {
   return (
-    <View>
-      <Text>Game Over!</Text>
+    <View style={styles.gameOver}>
+      <Title>GAME OVER</Title>
+      <View style={styles.foreground}>
+        <Image
+          source={require(IMAGE_FOREGROUND_PATH)}
+          style={styles.image}
+        />
+      </View>
+      <Text>Your phone needed X rounds to guess the number Y.</Text>
     </View>
   );
 }
--- src/screens/GameOver/styles.js
+++ src/screens/GameOver/styles.js
@@ -1,7 +1,29 @@
 // NPM Packages
 import { StyleSheet } from 'react-native';
 
+// Colors
+import colors from './../../constants/colors';
+
 const styles = StyleSheet.create({
+  foreground: {
+    borderColor: colors.primary800,
+    borderRadius: 150,
+    borderWidth: 3,
+    height: 300,
+    overflow: 'hidden',
+    margin: 36,
+    width: 300,
+  },
+  gameOver: {
+    alignItems: 'center',
+    flex: 1,
+    justifyContent: 'center',
+    padding: 24,
+  },
+  image: {
+    height: '100%',
+    width: '100%',
+  },
 });
 
 export default styles;
